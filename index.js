fd7a003b722f4a530f7521a876610724e58211f0
geamt <f201506100110137@gmail.com>
2019-07-21 14:23:35 +0530

    initialization

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var mysql = require('mysql');
+var bodyparser = require('body-parser');
+
+var app = express();
+
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+app.listen(3000,() => {
+    console.log('nodejs serve running');
+})
+
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'game'
+})
+
+app.post('/agg_game',function(req,res,next){
+    var data = JSON.parse(req.body.data);
+    var name = data.name;
+    var cricketer = data.cricketer;
+    var colors = data.colors;
+
+
+    connection.connect(function(){
+        query = "insert into tbl_game(player_name,cricketer,flag_color,date) values("+name+","+cricketer+","+colors+","+new Date()+")";
+
+        connection.query(query,function(err,result){
+            if(err)
+            {
+                res.end(JSON.stringify(err));
+            }
+            else
+            {
+                if(result.affectedRows > 0)
+                {
+                    res.end('successfully game added');
+                }
+                else
+                {
+                    res.end('Please try again');
+                }
+            }
+        })
+})
+})
