fe8ff19daca2ce84d3a99f906bb53d31d9ca3daf
geamt <f201506100110137@gmail.com>
2019-07-21 14:31:59 +0530

    update

--- index.js
+++ index.js
@@ -5,46 +5,59 @@ var bodyparser = require('body-parser');
 var app = express();
 
 
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log('nodejs serve running');
 })
 
 var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'game'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'game'
 })
 
-app.post('/agg_game',function(req,res,next){
+app.post('/agg_game', (req, res, next) => {
     var data = JSON.parse(req.body.data);
     var name = data.name;
     var cricketer = data.cricketer;
     var colors = data.colors;
 
 
-    connection.connect(function(){
-        query = "insert into tbl_game(player_name,cricketer,flag_color,date) values("+name+","+cricketer+","+colors+","+new Date()+")";
+    connection.connect(function () {
+        query = "insert into tbl_game(player_name,cricketer,flag_color,date) values(" + name + "," + cricketer + "," + colors + "," + new Date() + ")";
 
-        connection.query(query,function(err,result){
-            if(err)
-            {
+        connection.query(query, function (err, result) {
+            if (err) {
                 res.end(JSON.stringify(err));
             }
-            else
-            {
-                if(result.affectedRows > 0)
-                {
+            else {
+                if (result.affectedRows > 0) {
                     res.end('successfully game added');
                 }
-                else
-                {
+                else {
                     res.end('Please try again');
                 }
             }
         })
+    })
 })
+
+app.get('/getdata', (req, res, next) => {
+    connection.connect(function () {
+        query = "select * from tbl_game";
+
+        connection.query(query, function (err, result) {
+            if (err) {
+                res.end(JSON.stringify(err));
+            }
+            else {
+                res.json({
+                    data : result
+                })
+            }
+        })
+    })
 })
